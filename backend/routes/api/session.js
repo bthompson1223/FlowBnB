cdb5fa57b1a74b7ff35630af62ab9d47a154c947
Bryan Thompson <bthompson1223@outlook.com>
2023-12-21 15:37:19 -0600

    Add User input validation on user signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -38,7 +38,7 @@ router.get("/", (req, res) => {
 });
 
 // Log in
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.unscoped().findOne({
