c54bb9452bcddc46d229a4c4641ed12ef510d24d
Bryan Thompson <bthompson1223@outlook.com>
2024-01-02 14:20:01 -0600

    Finish get all spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const { Spot, Review, SpotImage } = require("../../db/models");
+const { requireAuth } = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
   const spots = await Spot.findAll();
+  let results = [];
+  for (let i = 0; i < spots.length; i++) {
+    currentSpot = { ...spots[i].dataValues };
+    const totalReviews = await Review.count({
+      where: {
+        spotId: spots[i].dataValues.id,
+      },
+    });
+    const reviewSum = await Review.sum("stars", {
+      where: {
+        spotId: spots[i].dataValues.id,
+      },
+    });
+    const reviewAvg = reviewSum / totalReviews;
+
+    const previewImage = await SpotImage.findOne({
+      where: {
+        spotId: spots[i].dataValues.id,
+      },
+    });
+
+    currentSpot.avgRating = reviewAvg;
+    currentSpot.previewImage = previewImage.url;
+    results.push(currentSpot);
+  }
+  res.json({
+    Spots: results,
+  });
+});
+
+router.get("/current", requireAuth, async (req, res) => {
+  const { user } = req;
+  const spots = await Spot.findAll({
+    where: {
+      ownerId: user.id,
+    },
+  });
   let results = [];
   for (let i = 0; i < spots.length; i++) {
     currentSpot = { ...spots[i].dataValues };
